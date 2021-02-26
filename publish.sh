501c86ce3268289fa1b7efcddc22ca474efb1c3c
LoveIsGrief <loveisgrief@tuta.io>
2021-02-26 23:16:06 +0100

    ci: Pass correct params to ssh publisher
    #8 - Find automatic distribution method

--- publish.sh
+++ publish.sh
@@ -6,10 +6,17 @@ ls web-ext-artifacts/*.xpi
 
 xpifile="$(ls web-ext-artifacts/*.xpi)"
 
+version=$(echo "
+  console.log((`cat manifest.json`).version)
+" | node)
+addonID=$(echo "
+  console.log((`cat manifest.json`).applications.gecko.id)
+" | node)
+
 # Pin the extension to IPFS
 ssh -o StrictHostKeyChecking=no \
     -p "$PIN_SSH_PORT" \
     "$PIN_SSH_USER@$PIN_SSH_HOST" \
-    "$xpifile" < "web-ext-artifacts/$xpifile" | tee hash
+    "$addonID" "$version" < "web-ext-artifacts/$xpifile" | tee hash
 
 
